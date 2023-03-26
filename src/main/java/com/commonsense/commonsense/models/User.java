6d336d29c73de0d989d45ba49d2bf23697be56fe
Steve <stevedee97@gmail.com>
2023-03-26 12:01:29 -0500

    User model created

--- src/main/java/com/commonsense/commonsense/models/User.java
+++ src/main/java/com/commonsense/commonsense/models/User.java
@@ -8,6 +8,9 @@ import javax.validation.constraints.Size;
 
 import java.time.LocalDateTime;
 
+
+// ORM Databased Created //
+
 @Entity
 @Table(name = "users")
 public class User {
@@ -38,17 +41,76 @@ public class User {
 
     @Column(name = "created_at", nullable = false, updatable = false)
     @CreationTimestamp
-    private LocalDateTime CreatedAt;
+    private LocalDateTime createdAt;
 
     @Column(name = "updated_at", nullable = false)
     @UpdateTimestamp
     private LocalDateTime updatedAt;
 
-
     // Getters and Setters //
 
-    private int Id;
-    private String Email;
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPartyAffiliation() {
+        return partyAffiliation;
+    }
+
+    public void setPartyAffiliation(String partyAffiliation) {
+        this.partyAffiliation = partyAffiliation;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 
 }
