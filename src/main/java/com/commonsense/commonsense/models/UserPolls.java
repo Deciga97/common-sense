9b3eb5e061485687c0f0222ad88b85d8465ed29a
Steve <stevedee97@gmail.com>
2023-03-27 20:33:01 -0500

    User Polls ORM table created

--- src/main/java/com/commonsense/commonsense/models/UserPolls.java
+++ src/main/java/com/commonsense/commonsense/models/UserPolls.java
@@ -1,4 +1,36 @@
 package com.commonsense.commonsense.models;
 
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+
+// ORM Table Created //
+
+@Entity
+@Table(name = "user_polls")
 public class UserPolls {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @Column(name = "poll_type", nullable = false, length = 50)
+    private String pollType;
+
+    @Column(name = "poll_id", nullable = false)
+    private Long pollId;
+
+    @Column(name = "vote", nullable = false, length = 10)
+    private String vote;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
