177e5dcc8dcc7d42f5208c6db1f60254fd310a7e
Steve <stevedee97@gmail.com>
2023-03-28 09:39:20 -0500

    ORM table created

--- src/main/java/com/commonsense/commonsense/models/Lobbyist.java
+++ src/main/java/com/commonsense/commonsense/models/Lobbyist.java
@@ -1,4 +1,48 @@
 package com.commonsense.commonsense.models;
 
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+//ORM Table Created //
+
+@Entity
+@Table(name = "lobbyist")
 public class Lobbyist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false, length = 50)
+    private String firstName;
+
+    @Column(name = "middle_name", length = 50)
+    private String middleName;
+
+    @Column(name = "last_name", nullable = false, length = 50)
+    private String lastName;
+
+    @Column(name = "organization", nullable = false, length = 255)
+    private String organization;
+
+    @Column(name = "email", length = 255)
+    private String email;
+
+    @Column(name = "phone_number", length = 50)
+    private String phoneNumber;
+
+    @Column(name = "office_address", length = 255)
+    private String officeAddress;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
 }
