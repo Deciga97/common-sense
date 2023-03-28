ed4b1a44923b916bba1ca41a3be3685fb2387745
Steve <stevedee97@gmail.com>
2023-03-27 19:55:13 -0500

    Politc-voting-records constructors created

--- src/main/java/com/commonsense/commonsense/models/PoliticianVotingRecord.java
+++ src/main/java/com/commonsense/commonsense/models/PoliticianVotingRecord.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
 
-// ORM Database Created //
+// ORM Table Created //
 @Entity
 @Table(name = "politician_voting_records")
 public class PoliticianVotingRecord {
@@ -30,10 +30,22 @@ public class PoliticianVotingRecord {
     @Column(name = "updated_at", nullable = false, updatable = false)
     private LocalDateTime updatedAt;
 
+    // Constructors //
 
+    public Politician getPolitician() {}
 
+    public PoliticianVotingRecord() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
 
-
+    public PoliticianVotingRecord(Bill bill, Politician politician, String vote) {
+        this.bill = bill;
+        this.politician = politician;
+        this.vote = vote;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
 
 
 }
