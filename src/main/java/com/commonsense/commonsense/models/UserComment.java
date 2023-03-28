613ff1d930a520595480fd0620cd3feeee87f3ef
Steve <stevedee97@gmail.com>
2023-03-27 21:24:17 -0500

    User Comment ORM table created

--- src/main/java/com/commonsense/commonsense/models/UserComment.java
+++ src/main/java/com/commonsense/commonsense/models/UserComment.java
@@ -1,4 +1,32 @@
 package com.commonsense.commonsense.models;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+// ORM Table Created //
+
+@Entity
+@Table(name = "user_comments")
 public class UserComment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name ="user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "bill_id", nullable = false)
+    private Bill bill;
+
+    @Column(name = "text", nullable = false, length = 1000)
+    private String text;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+
 }
