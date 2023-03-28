52e46699f438adc3e5ef4cafc3d1a15b1c0aab5e
Steve <stevedee97@gmail.com>
2023-03-28 09:27:32 -0500

    Remodify donation ORM table

--- src/main/java/com/commonsense/commonsense/models/Donation.java
+++ src/main/java/com/commonsense/commonsense/models/Donation.java
@@ -1,7 +1,11 @@
 package com.commonsense.commonsense.models;
 
 import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
@@ -13,23 +17,48 @@ public class Donation {
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name = "user_id", referencedColumnName = "id")
-    private User user;
+    @JoinColumn(name = "politician_id", referencedColumnName = "id")
+    private Politician politician;
 
-    @Column(name = "type", nullable = false, length = 50)
-    private String type;
+    @Column(name = "donor_name", nullable = false, length = 255)
+    private String donorName;
 
-    @Column(name = "description", nullable = false, length = 255)
-    private String description;
+    @Column(name = "amount", nullable = false, precision = 10, scale = 2)
+    private BigDecimal amount;
 
-    @Column(name = "created_at", nullable = false, length = 255)
-    private String description;
+    @Column(name = "donation_date", nullable = false)
+    private LocalDate donationDate;
 
-    @Column(name = "read", nullable = false)
-    private boolean read;
+    @Column(name = "donation_type", nullable = false, length = 50)
+    private String donationType;
 
-    @Column(name = "read_at")
-    private LocalDateTime readAt;
+    @Column(name = "occupation", length = 255)
+    private String occupation;
+
+    @Column(name = "employer", length = 255)
+    private String employer;
+
+    @Column(name = "address", length = 255)
+    private String address;
+
+    @Column(name = "city", length = 50)
+    private String city;
+
+    @Column(name = "state", length = 2)
+    private String state;
+
+    @Column(name = "zip_code", length = 10)
+    private String zipCode;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+
 
 
 
