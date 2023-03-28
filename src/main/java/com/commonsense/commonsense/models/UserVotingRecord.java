908f0973d4a1c66638076aec73f4775da8b997da
Steve <stevedee97@gmail.com>
2023-03-27 20:21:36 -0500

    User Voting Record ORM table created

--- src/main/java/com/commonsense/commonsense/models/UserVotingRecord.java
+++ src/main/java/com/commonsense/commonsense/models/UserVotingRecord.java
@@ -1,4 +1,36 @@
 package com.commonsense.commonsense.models;
 
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+
+// ORM Table Created//
+
+@Entity
+@Table(name = "user_voting_records")
 public class UserVotingRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "poll_id", nullable = false)
+    private Poll poll;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "option_id", nullable = false)
+    private PollOption option;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
 }
