d9bb6c2b9ff8b9031a56d84b805d5b339437d187
Steve <stevedee97@gmail.com>
2023-03-27 19:53:11 -0500

    polit-voting-record ORM table created

--- src/main/java/com/commonsense/commonsense/models/PoliticianVotingRecord.java
+++ src/main/java/com/commonsense/commonsense/models/PoliticianVotingRecord.java
@@ -1,4 +1,39 @@
 package com.commonsense.commonsense.models;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+// ORM Database Created //
+@Entity
+@Table(name = "politician_voting_records")
 public class PoliticianVotingRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "bill_id")
+    private Bill bill;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "politician_id")
+    private Politician politician;
+
+    @Column(name = "vote", length = 10, nullable = false)
+    private String vote;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false, updatable = false)
+    private LocalDateTime updatedAt;
+
+
+
+
+
+
+
 }
