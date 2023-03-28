00bae5954632522bebfddfa4ed1e2557b2d7b384
Steve <stevedee97@gmail.com>
2023-03-27 21:45:33 -0500

    Notification ORM table created

--- src/main/java/com/commonsense/commonsense/models/Notification.java
+++ src/main/java/com/commonsense/commonsense/models/Notification.java
@@ -1,4 +1,37 @@
 package com.commonsense.commonsense.models;
 
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+// ORM Table Created //
+@Entity
+@Table(name = "notifications")
 public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "type", nullable = false, length = 50)
+    private String type;
+
+    @Column(name = "description", nullable = false, length = 50)
+    private String description;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "read", nullable = false)
+    private boolean read;
+
+    @Column(name = "read_at")
+    private LocalDateTime readAt;
+
+
 }
