3dad553299070b44ac3eb33f13f727c1c3343cc2
Steve <stevedee97@gmail.com>
2023-03-27 21:47:08 -0500

    Notification constructors/getters and setters created

--- src/main/java/com/commonsense/commonsense/models/Notification.java
+++ src/main/java/com/commonsense/commonsense/models/Notification.java
@@ -33,5 +33,76 @@ public class Notification {
     @Column(name = "read_at")
     private LocalDateTime readAt;
 
+    // Constructors //
 
+    public Notification() {}
+
+    public Notification(Long id, User user, String type, String description, LocalDateTime createdAt, boolean read, LocalDateTime readAt) {
+        this.id = id;
+        this.user = user;
+        this.type = type;
+        this.description = description;
+        this.createdAt = createdAt;
+        this.read = read;
+        this.readAt = readAt;
+    }
+
+    // Getters and Setters //
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public boolean isRead() {
+        return read;
+    }
+
+    public void setRead(boolean read) {
+        this.read = read;
+    }
+
+    public LocalDateTime getReadAt() {
+        return readAt;
+    }
+
+    public void setReadAt(LocalDateTime readAt) {
+        this.readAt = readAt;
+    }
 }
