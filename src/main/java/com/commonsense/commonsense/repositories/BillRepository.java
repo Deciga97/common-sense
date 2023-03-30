4c5b768e447069797a01828d67b22c5d9f3e8dc7
Steve <stevedee97@gmail.com>
2023-03-30 12:48:53 -0500

    Bill-repo extension to JPA-repo created

--- src/main/java/com/commonsense/commonsense/repositories/BillRepository.java
+++ src/main/java/com/commonsense/commonsense/repositories/BillRepository.java
@@ -1,4 +1,7 @@
 package com.commonsense.commonsense.repositories;
 
-public interface BillRepository {
+import com.commonsense.commonsense.models.Bill;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BillRepository extends JpaRepository<Bill, Long> {
 }
