ba44b513caf20954468fe4e7f8f3a9d6af0b7979
Steve <stevedee97@gmail.com>
2023-04-01 15:02:45 -0500

    donation class abstract methods created

--- src/main/java/com/commonsense/commonsense/Services/DonationService.java
+++ src/main/java/com/commonsense/commonsense/Services/DonationService.java
@@ -1,4 +1,13 @@
 package com.commonsense.commonsense.Services;
 
+import com.commonsense.commonsense.models.Donation;
+
+import java.util.List;
+
 public interface DonationService {
+    public abstract Donation createDonation(Donation donation);
+    public abstract Donation updateDonation(Long id, Donation donation);
+    public abstract void deleteDonation(Long id);
+    public abstract Donation getDonationById(Long id);
+    public abstract List<Donation> getAllDonations();
 }
