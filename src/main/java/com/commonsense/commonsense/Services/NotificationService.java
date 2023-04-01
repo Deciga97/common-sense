b66a82f1933456471dba73aa07764b7043b919fd
Steve <stevedee97@gmail.com>
2023-04-01 15:17:04 -0500

    Notification service abstract code created

--- src/main/java/com/commonsense/commonsense/Services/NotificationService.java
+++ src/main/java/com/commonsense/commonsense/Services/NotificationService.java
@@ -1,5 +1,18 @@
 package com.commonsense.commonsense.Services;
 
-public interface NotificationService {
+import com.commonsense.commonsense.models.Notification;
+import com.commonsense.commonsense.models.User;
 
+import java.util.List;
+
+public interface NotificationService {
+    Notification createNotification(Notification notification);
+
+    Notification updateNotification(Notification notification);
+
+    void deleteNotification(Long id);
+
+    List<Notification> getAllNotifications();
+
+    List<Notification> getNotificationsByUser(User user);
 }
