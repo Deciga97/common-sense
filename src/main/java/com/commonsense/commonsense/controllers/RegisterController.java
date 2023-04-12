6f5320d5d38877171fd073529865320ae7d68cf4
Steve <stevedee97@gmail.com>
2023-04-12 10:54:11 -0500

    Mapping register

--- src/main/java/com/commonsense/commonsense/controllers/RegisterController.java
+++ src/main/java/com/commonsense/commonsense/controllers/RegisterController.java
@@ -1,4 +1,12 @@
 package com.commonsense.commonsense.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class RegisterController {
+    @GetMapping("/register")
+    public String getMapping() {
+        return "register";
+    }
 }
